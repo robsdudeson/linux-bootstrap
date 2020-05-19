62e586a2c0e6ec3a1733b0d8ad6734ef3326c28b
Robby Thompson <robsdudeson@gmail.com>
2020-05-18 23:14:40 -0400

    need to be sudo

--- apt/pre-reqs.sh
+++ apt/pre-reqs.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-set -o errexit
+set -e
 
 sudo apt install -y curl wget apt-transport-https dirmngr
 
 wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb && dpkg -i deb-multimedia-keyring_2016.8.1_all.deb
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F24AEA9FB05498B7
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F24AEA9FB05498B7
  

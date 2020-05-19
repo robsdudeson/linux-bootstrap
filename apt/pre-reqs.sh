8166fb82655a28121722a3dd9c46a6bc50d18b30
Robby Thompson <robsdudeson@gmail.com>
2020-05-18 23:18:22 -0400

    really be sudo

--- apt/pre-reqs.sh
+++ apt/pre-reqs.sh
@@ -4,8 +4,7 @@ set -e
 
 sudo apt install -y curl wget apt-transport-https dirmngr
 
-wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb && dpkg -i deb-multimedia-keyring_2016.8.1_all.deb
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb
+sudo dpkg -i deb-multimedia-keyring_2016.8.1_all.deb
+sudo wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F24AEA9FB05498B7
- 
